8f0a8fce84d6012a027a40166d4d46261792b6f3
Satoshi Ohmori <sachin21dev@gmail.com>
2015-09-09 18:43:23 +0900

    Fix title for spec

--- spec/space2dash_spec.rb
+++ spec/space2dash_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Space2dash do
-  it 'should be Successful execution' do
+  it 'returns dash included in string' do
     expect(Space2dash.convert(['fuga hoge foo'])).to include('-') # String case
     expect(Space2dash.convert(%w(fuga hoge foo))).to include('-') # Array case
   end
