71568625b8d664fca13d76ba21e272f0d4e05075
Satoshi Ohmori <sachin21dev@gmail.com>
2015-09-27 01:35:53 +0900

    Do not use 'class << self'

--- lib/space2dash.rb
+++ lib/space2dash.rb
@@ -1,13 +1,11 @@
 require 'space2dash/version'
 
 module Space2dash
-  class << self
-    def convert(argv)
-      argv.length == 1 ? argv[0].strip.gsub(/\s/,  '-') : argv.join('-')
-    end
+  def self.convert(argv)
+    argv.length == 1 ? argv[0].strip.gsub(/\s/,  '-') : argv.join('-')
+  end
 
-    def create_new_branch(dash_include_sentence)
-      system "git checkout -b #{dash_include_sentence}"
-    end
+  def self.create_new_branch(dash_include_sentence)
+    system "git checkout -b #{dash_include_sentence}"
   end
 end
